1263723a803100477fa0265846a9b8e6205a4656
Abhiek187 <achaudhuri2011@yahoo.com>
2021-07-07 10:31:49 -0400

    Use act() from react testing library

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 let titleHeading: HTMLHeadingElement;
